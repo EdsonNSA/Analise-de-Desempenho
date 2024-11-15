afe2782230f8522011819cbba5f784f58ef49762
Edson <Edsonnunes360@gmail.com>
2024-11-15 20:08:38 -0300

    Adicionado 2 novas linguagens

---
+++ Leitor_TXT.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const util = require('util');
+
+function lerArquivo() {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readline.question("Digite o nome do arquivo: ", (nomeDoArquivo) => {
+        try {
+            const conteudo = fs.readFileSync(nomeDoArquivo, 'utf-8').split('\n');
+            const lista = conteudo[0].trim().split(' ').map(Number);
+            const acoes = parseInt(conteudo[1].trim());
+
+            for (let i = 2; i < 2 + acoes; i++) {
+                const acao = conteudo[i].trim().split(' ');
+                const comando = acao[0];
+
+                if (comando === 'A') {
+                    const numero = parseInt(acao[1]);
+                    const posicao = parseInt(acao[2]);
+                    lista.splice(posicao, 0, numero);
+                } else if (comando === 'R') {
+                    const numero = parseInt(acao[1]);
+                    const index = lista.indexOf(numero);
+                    if (index !== -1) lista.splice(index, 1);
+                } else if (comando === 'P') {
+                    // Usei o util.inspect para exibir o array completo
+                    console.log(util.inspect(lista, { maxArrayLength: null }));
+                }
+            }
+        } catch (error) {
+            console.log(`O arquivo ${nomeDoArquivo} não foi encontrado.`);
+        } finally {
+            readline.close();
+        }
+    });
+}
+
+lerArquivo();
---
+++ Leitor_TXT.php
@@ -0,0 +1,37 @@
+<?php
+function lerArquivo() {
+    echo "Digite o nome do arquivo: ";
+    $nomeDoArquivo = trim(fgets(STDIN));
+
+    if (!file_exists($nomeDoArquivo)) {
+        echo "O arquivo $nomeDoArquivo não foi encontrado.\n";
+        return;
+    }
+
+    $linhas = file($nomeDoArquivo);
+
+    $lista = array_map('intval', explode(' ', trim($linhas[0])));
+    $acoes = intval($linhas[1]);
+
+    for ($i = 2; $i < 2 + $acoes; $i++) {
+        $acao = $linhas[$i];
+        $partes = explode(' ', $acao);
+        $comando = $partes[0];
+
+        if ($comando === 'A') {
+            $numero = intval($partes[1]);
+            $posicao = intval($partes[2]);
+            array_splice($lista, $posicao, 0, $numero);
+        } elseif ($comando === 'R') {
+            $numero = intval($partes[1]);
+            $index = array_search($numero, $lista);
+            if ($index !== false) {
+                array_splice($lista, $index, 1);
+            }
+        } elseif ($comando === 'P') {
+            print_r($lista);
+        }
+    }
+}
+
+lerArquivo();
